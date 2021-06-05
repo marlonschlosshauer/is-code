9881c53fedae97b8ace0c399af9baea74a6760c8
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-06-05 23:24:13 +0200

    add cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyparser = require('body-parser');
 const app = express();
 const auth = require('./routes/auth');
@@ -6,6 +7,7 @@ const resources = require('./routes/resources');
 
 const port = 3000;
 
+app.use(cors());
 app.use(bodyparser.json())
 app.use('/', auth);
 app.use('/api', resources);
