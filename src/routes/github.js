9169f82638de8dd681c894fec93542cda3e3c531
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-06-08 15:52:10 +0200

    fix minor

--- src/routes/github.js
+++ src/routes/github.js
@@ -9,7 +9,6 @@ passport.serializeUser((user, cb) => {
 	cb(null, user);
 })
 
-
 passport.deserializeUser((user, cb) => {
 	cb(null, user);
 })
@@ -25,14 +24,12 @@ passport.use(new Github({
 	}
 ));
 
-
 router.get('/',
 	passport.authenticate('github', { scope: ['user:email'] }));
 
 router.get('/callback',
 	passport.authenticate('github', { failureRedirect: '/login' }),
 	function(req, res) {
-		// Successful authentication, redirect home.
 		res.redirect('/');
 	});
 
